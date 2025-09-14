f4eae1119c56797d646de0c5fafc14ba0d45861b
Silvio Guerreiro <silvio.guerreirojunior@gmail.com>
2025-09-13 21:31:32 -0400

    importação de bibliotecas e criação de estruturas iniciais

--- farm.py
+++ farm.py
@@ -1,1 +1,16 @@
-teste
+import math
+import csv
+import os
+
+# Item d.
+culturas_data = {
+    "soja": [],
+    "milho": [],
+}
+
+produtos_disponiveis = {
+    "1": "Fosfato",
+    "2": "Ureia",
+    "3": "Cloreto de Potássio",
+    "4": "Calcário"
+}
